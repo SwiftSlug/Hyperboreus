b9683a87e5b769ca988914c56596981b135d7bfa
dbarnes94 <14029375@students.southwales.ac.uk>
2017-10-07 20:30:23 +0100

    Basic Commit
    Added in a few more starter objects such as collision for the player
    Networking for the player
    Fixes for the camera when having several players

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,36 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.Networking;
 
-public class PlayerController : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
+public class PlayerController : NetworkBehaviour
+{
+	//Use this for initialization
+	void Start()
+	{
+		attachCamera();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+	//Update is called once per frame
+	[Client]
+	void Update()
+	{
+		//if(this.isServer)
+		//{
+		//	return;
+		//}
+
+		var xAxis = Input.GetAxis("Horizontal") * 1.5f;
+		var yAxis = Input.GetAxis("Vertical") * 0.1f;
+
+		transform.Rotate(0, xAxis, 0);
+		transform.Translate(0, 0, yAxis);
+	}
+
+	void attachCamera()
+	{
+		if(this.isLocalPlayer)
+		{
+			GameObject.FindGameObjectWithTag("MainCamera").transform.parent = this.transform; //Attach the MainCamera as a child to the player
+			GameObject.FindGameObjectWithTag("MainCamera").transform.position = transform.Find("DummyCam").position; //Transform the position of the MainCamera to the DummyCam
+		}
 	}
 }

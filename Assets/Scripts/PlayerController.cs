5dbdb9e06f82139158cceda8926a0e6e34bd590a
Darrell Barnes <darrell.b1994@gmail.com>
2017-10-12 18:13:40 +0300

    Fixed networked camera
    Network camera now fixed via a use of old method of networked camera and use of local player checking.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,35 +3,52 @@ using UnityEngine.Networking;
 
 public class PlayerController : NetworkBehaviour
 {
-	Vector3 playerOrientation;
-	public Camera myCamera;
+	Vector3 playerOrientation; //Player Orientation
+	public GameObject myCamera; //Camera Reference
+    Vector3 heightOffset; //Camera height from player
+    Vector3 verticalOffset; //Camera vertical alignment from player
+
+    void Start()
+    {
+        if (this.isLocalPlayer)
+        {
+           myCamera = GameObject.FindGameObjectWithTag("MainCamera"); //Set a reference to the player
+           heightOffset = new Vector3(0.0f, 12.0f, 0.0f); //Give our camera a certain height to offset - could be exposed to editor
+           verticalOffset = new Vector3(0.0f, 0.0f, -2.0f); //Give our camera a certain vertical offset - could be exposed to editor
+        }
+    }
 
 	//Update is called once per frame
 	void Update()
 	{
 		if (!this.isLocalPlayer)
 		{
-			myCamera.enabled = false;
 			return;
 		}
 
-		Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+		Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition); //Cast a ray from the main camera relative to the screen position
 
-		RaycastHit mouseRayHit;
+		RaycastHit mouseRayHit; //Reference to the ray hit - used in calculating player orientation
 
-		if (Physics.Raycast(mouseRay, out mouseRayHit, 100))
+		if (Physics.Raycast(mouseRay, out mouseRayHit, 100)) //If the Ray has hit an object
 		{
-			playerOrientation = mouseRayHit.point;
+			playerOrientation = mouseRayHit.point; //Make our potential player orientation equal to the location we hit with the ray cast.
 		}
 
-		Vector3 playerDirection = playerOrientation - transform.position;
-		playerDirection.y = 0;
+		Vector3 playerDirection = playerOrientation - transform.position; //Save a vector based on our players position and the saved orientation
+		playerDirection.y = 0; //Get rid of our y portion of the vector because we don't need it
 
-		var xAxis = Input.GetAxis("Horizontal") * 0.1f;
-		var yAxis = Input.GetAxis("Vertical") * 0.1f;
+		var xAxis = Input.GetAxis("Horizontal") * 0.1f; //Get the input horizontally and save it. "W,S,Up,Down,Joystick Up,Joystick Down"
+		var yAxis = Input.GetAxis("Vertical") * 0.1f; //Get the vertical input and and save it. "A,D,Left,Right,Joystick Left, Joystick Right"
 
-		transform.LookAt(transform.position + playerDirection, Vector3.up);
-		transform.Translate(Vector3.forward * yAxis, Space.World);
-		transform.Translate(Vector3.right * xAxis, Space.World);
+		transform.LookAt(transform.position + playerDirection, Vector3.up); //Look at the mouse position in screen space
+		transform.Translate(Vector3.forward * yAxis, Space.World); //Move horizontally within world space instead of local
+		transform.Translate(Vector3.right * xAxis, Space.World); //Move vertically within world space instead of local
 	}
+
+    //Called after the scene has been updating
+    void LateUpdate()
+    {
+        myCamera.transform.position = (this.transform.position + heightOffset + verticalOffset); //Make sure our camera is following the player
+    }
 }

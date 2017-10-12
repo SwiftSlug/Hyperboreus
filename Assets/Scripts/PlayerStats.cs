44d702e13b651c005806cea510aa70091061beb9
dbarnes94 <14029375@students.southwales.ac.uk>
2017-10-12 22:58:46 +0100

    Added PlayerStats
    Script needs to be edited more to support more game features.

---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerStats : NetworkBehaviour
+{
+	public static int maxHealth = 100;
+
+	[SyncVar]
+	public int currentHealth = maxHealth;
+
+	public bool isDead = false;
+
+	public void Damage(int amount)
+	{
+		currentHealth -= amount;
+
+		if(currentHealth <= 0)
+		{
+			currentHealth = 0;
+			isDead = true;
+		}
+	}
+
+	public void Heal(int amount)
+	{
+		currentHealth += amount;
+
+		if (currentHealth >= maxHealth)
+		{
+			currentHealth = maxHealth;
+		}
+	}
+}

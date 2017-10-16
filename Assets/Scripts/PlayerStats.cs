13cfe934ada174e14287cce9bbb34babe5702b70
Darrell Barnes <darrell.b1994@gmail.com>
2017-10-16 12:18:05 +0300

    Re-Implemented Player Regen
    Regen now re-implemented.
    Player Kill function added.
    Player Revive function added.

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -13,24 +13,22 @@ public class PlayerStats : NetworkBehaviour
 	[SyncVar(hook = "ChangeHealth")]
 	public int currentHealth; //The player's current health, can go down from being damaged or up from being healed or regenerating.
 
-	//[Tooltip("Boolean for player death check")]
-	//[SyncVar]
-	//public bool isDead = false; //Used when determining if the player is dead and if they should be revived or respawned
+	[Tooltip("Boolean for player death check")]
+	[SyncVar]
+	public bool isDead = false; //Used when determining if the player is dead and if they should be revived or respawned
 
-	//[Tooltip("Time code at which player was damaged")]
-	//[SyncVar] 
-	//public float timeDamaged = 0.0f; //Time code at which player was damaged
-	//[Tooltip("Regen rate. Quarter of a second Default before each regen tick")]
-	//[SyncVar]
-	//public float regenHealthSpeed = 0.25f; //Regen rate. Quarter of a second Default before each regen tick
-	//[Tooltip("Time in (seconds)before regen kicks in. 10 Second Default")]
-	//[SyncVar]
-	//public float regenHealthDelay = 10.0f; //Time in (seconds) before regen kicks in. 10 Second Default
+	[Tooltip("Time code at which player was damaged")]
+	[SyncVar] 
+	public float timeDamaged = 0.0f; //Time code at which player was damaged
+	[Tooltip("Regen rate. Quarter of a second Default before each regen tick")]
+	[SyncVar]
+	public float regenHealthSpeed = 0.25f; //Regen rate. Quarter of a second Default before each regen tick
+	[Tooltip("Time in (seconds)before regen kicks in. 10 Second Default")]
+	[SyncVar]
+    public float regenHealthDelay = 10.0f; //Time in (seconds) before regen kicks in. 10 Second Default
 
 	void Start()
 	{
-
-
 		if (!isLocalPlayer)
 		{
 			Canvas playerUI = gameObject.GetComponentInChildren<Canvas>();
@@ -43,10 +41,10 @@ public class PlayerStats : NetworkBehaviour
 			return;
 		}
 
-        //InvokeRepeating("RegenHealth", 0.0f, regenHealthSpeed);
+        currentHealth = maxHealth;
 
-		currentHealth = maxHealth;
-	}
+        CmdStartRegen();
+    }
 
 	void Update()
 	{
@@ -54,11 +52,6 @@ public class PlayerStats : NetworkBehaviour
 		{
 			return;
 		}
-
-		//if (currentHealth <= 0)
-		//{
-		//	isDead = true;
-		//}
 
 		if(Input.GetKeyDown(KeyCode.O))
 		{
@@ -68,6 +61,14 @@ public class PlayerStats : NetworkBehaviour
 		{
 			CmdHeal(20);
 		}
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            CmdKill();
+        }
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            CmdRevive();
+        }
 	}
 
 	[Command]
@@ -78,41 +79,94 @@ public class PlayerStats : NetworkBehaviour
 			return;
 		}
 
-		currentHealth -= amount;
-		//timeDamaged = Time.time;
+        currentHealth -= amount;
+
+		timeDamaged = Time.time;
 
 		if (currentHealth <= 0)
 		{
-			currentHealth = 0;
+            CmdKill();
 		}
 	}
 
 	[Command]
 	public void CmdHeal(int amount)
 	{
-		//if (isDead == false)
-		//{
-		if(!isServer)
+		if (!isServer)
 		{
 			return;
 		}
-			currentHealth += amount;
 
-			if (currentHealth >= maxHealth)
-			{
-				currentHealth = maxHealth;
-			}
-		//}
+        if (isDead)
+        {
+            return;
+        }
+
+		currentHealth += amount;
+
+		if (currentHealth >= maxHealth)
+		{
+			currentHealth = maxHealth;
+		}
 	}
 
-	//[Command]
-	//public void CmdRegenHealth()
-	//{
-	//	if (currentHealth < maxHealth && Time.time > (timeDamaged + regenHealthDelay) && isDead == false)
-	//	{
-	//		CmdHeal(1);
-	//	}
-	//}
+    [Command]
+    public void CmdKill()
+    {
+        if (!isServer)
+        {
+            return;
+        }
+
+        isDead = true;
+
+        timeDamaged = Time.time;
+
+        currentHealth = 0;
+    }
+
+    [Command]
+    public void CmdRevive()
+    {
+        if (!isServer)
+        {
+            return;
+        }
+
+        isDead = false;
+
+        timeDamaged = Time.time;
+
+        currentHealth = 10;
+    }
+
+    [Command]
+    public void CmdStartRegen()
+    {
+        if (!isServer)
+        {
+            return;
+        }
+
+        InvokeRepeating("CmdRegenHealth", 0.0f, regenHealthSpeed);
+    }
+
+	[Command]
+	public void CmdRegenHealth()
+	{
+        if (!isServer)
+        {
+            return;
+        }
+
+        if (timeDamaged != 0.0)
+        {
+            if (currentHealth < maxHealth && Time.time > (timeDamaged + regenHealthDelay) && isDead == false)
+            {
+                CmdHeal(1);
+            }
+        }
+	}
 
 	void ChangeHealth(int currentHealth)
 	{

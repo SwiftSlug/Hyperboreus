0e966e4adad5289ac6c839a7b24f9b94692baec0
Darrell Barnes <darrell.b1994@gmail.com>
2017-10-19 16:47:37 +0300

    Main Menu Progress
    Created a custom gaussian blur shader to use on the background of the main menu.
    Blur Shader is customisable when applied to a material
    Added an FPS display feature for use on any scene.
    Basic scene created with snow for main menu background with applied blur.

---
+++ Assets/Scripts/CameraBlur.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class CameraBlur : MonoBehaviour
+{
+    public Material BlurMaterial;
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, BlurMaterial);
+    }
+}

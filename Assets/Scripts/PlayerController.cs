c505470d2fce7112cb2fb4c0cd6a6f76b3d34e73
Darrell Barnes <darrell.b1994@gmail.com>
2017-10-19 17:02:34 +0300

    Bug Fix for Issue #10
    Changed the player's movement to be dependant on the time since the last frame update.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -25,8 +25,8 @@ public class PlayerController : NetworkBehaviour
 		Vector3 playerDirection = playerOrientation - transform.position; //Save a vector based on our players position and the saved orientation
 		playerDirection.y = 0; //Get rid of our y portion of the vector because we don't need it
 
-		var xAxis = Input.GetAxis("Horizontal") * 0.1f; //Get the input horizontally and save it. "W,S,Up,Down,Joystick Up,Joystick Down"
-		var yAxis = Input.GetAxis("Vertical") * 0.1f; //Get the vertical input and and save it. "A,D,Left,Right,Joystick Left, Joystick Right"
+		float xAxis = Input.GetAxis("Horizontal") * Time.deltaTime * 6.0f; //Get the input horizontally and save it. "W,S,Up,Down,Joystick Up,Joystick Down"
+		float yAxis = Input.GetAxis("Vertical") * Time.deltaTime * 6.0f; //Get the vertical input and and save it. "A,D,Left,Right,Joystick Left, Joystick Right"
 
 		transform.LookAt(transform.position + playerDirection, Vector3.up); //Look at the mouse position in screen space
 		transform.Translate(Vector3.forward * yAxis, Space.World); //Move horizontally within world space instead of local

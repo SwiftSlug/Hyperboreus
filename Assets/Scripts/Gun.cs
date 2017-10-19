a07abd0c3afe8feab397bfd42e1987ea374d4776
LuisWilliams95 <luis_1995@hotmail.co.uk>
2017-10-19 17:49:33 +0100

    Gun with effects

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float damagePerShot = 10f;               //Damage each bullet deals to enemies.
+    public float timeBetweenShots = 0.15f;          //The time between each shot.
+    public float range = 100f;                      //The range that the gun can fire/
+
+    float timer;                                    //Timer to know when you can shoot (used for 'timeBetweenShots').
+    Ray shootRay = new Ray();                       //Ray from the gun.
+    RaycastHit shootHit;                            //Raycast hit to determine what was hit.                           
+    ParticleSystem gunParticles;                    //Reference to the particle system.
+    LineRenderer gunLine;                           //Reference to the line renderer.
+    //AudioSource gunAudio;
+    Light gunLight;                                 //Reference to the guns light source.
+    float effectsDisplayTime = 0.2f;                //The proportion of the timeBetweenShots that the effects which display for.
+
+    void Awake()
+    {
+        // Create a layer mask for the Shootable layer.
+        //shootableMask = LayerMask.GetMask("Shootable");
+
+        // Set up the references.
+        gunParticles = GetComponent<ParticleSystem>();
+        gunLine = GetComponent<LineRenderer>();
+        //gunAudio = GetComponent<AudioSource>();
+        gunLight = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        timer += Time.deltaTime;
+
+        if (Input.GetButton("Fire1") && timer >= timeBetweenShots && Time.timeScale != 0)
+        {
+            Shoot();
+        }
+
+        // If the timer has exceeded the proportion of timeBetweenBullets that the effects should be displayed for...
+        if (timer >= timeBetweenShots * effectsDisplayTime)
+        {
+            // ... disable the effects.
+            DisableEffects();
+        }
+    }
+
+    public void DisableEffects()
+    {
+        // Disable the line renderer and the light.
+        gunLine.enabled = false;
+        gunLight.enabled = false;
+    }
+
+        void Shoot()
+    {
+        //Resets the timer.
+        timer = 0f; 
+
+        //gunAudio.Play();
+
+        //Turns on gun flash (light source).
+        gunLight.enabled = true;
+
+        //Stops particles playing if they were already, then starts playing again.
+        gunParticles.Stop();
+        gunParticles.Play();
+
+        //Enable the line renderer and set its position to the gun.
+        gunLine.enabled = true;
+        gunLine.SetPosition(0, transform.position);
+
+        //Sets the shootRay so it starts at the gun and points forward.
+        shootRay.origin = transform.position;
+        shootRay.direction = transform.forward;
+
+        if (Physics.Raycast(shootRay, out shootHit, range))
+        {
+            //EnemyHealth enemyHealth = shootHit.collider.GetComponent<EnemyHealth>();
+            //if (enemyHealth != null)
+            //{
+            //    enemyHealth.TakeDamage(damagePerShot, shootHit.point);
+            //}
+            gunLine.SetPosition(1, shootHit.point);
+        }
+        else
+        {
+            gunLine.SetPosition(1, shootRay.origin + shootRay.direction * range);
+        }
+    }
+}

d10bdeb5a02eb002fbb8768ecc60865a7478d116
dbarnes94 <14029375@students.southwales.ac.uk>
2017-10-20 16:10:30 +0100

    Main Menu Changes
    Added temporary title card to the title menu. Better graphic should be used in future.
    Scene can transition from the title menu using a developed reusable button script.
    Main Menu camera field of view changed as it was too narrow. More ground planes added to adjust for this.
    Blur effect changed slightly to split the scene less.
    Menu buttons started - Application can quit and go to a multiplayer lobby

---
+++ Assets/Scripts/ButtonLoadScene.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonLoadScene : MonoBehaviour
+{
+	public void LoadScene(int index)
+	{
+		Scene(index);
+	}
+
+	public void LoadSceneAsync(int index)
+	{
+		StartCoroutine(SceneAsync(index));
+	}
+
+	public void QuitApplication()
+	{
+		Application.Quit();
+	}
+
+	private void Scene(int index)
+	{
+		SceneManager.LoadScene(index);
+	}
+
+	IEnumerator SceneAsync(int sceneIndex)
+	{
+		AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneIndex);
+
+		while (!asyncLoad.isDone)
+		{
+			yield return null;
+		}
+	}
+}

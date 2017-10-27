3369e4fbb4d7b68226cc4aa7727039b4afaa7f63
liampaddick <liampaddick2207@outlook.com>
2017-10-27 15:10:51 +0100

    Added scripts and prefabs neccessary for networking
    Added grid spawning and grid collider prefabs. Added scripts for the neccessary assets as well.

--- Assets/Scripts/BaseBuildingPlayerScript.cs
+++ Assets/Scripts/BaseBuildingPlayerScript.cs
@@ -1,16 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class BaseBuildingPlayerScript : MonoBehaviour {
+public class BaseBuildingPlayerScript : NetworkBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
+    public Transform Object;
+    public GameObject StructurePrefab;
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+		if (Input.GetKeyDown ("b"))
+        {
+            StructurePrefab = (GameObject)Instantiate(StructurePrefab, Object.position, Object.rotation);
+            NetworkServer.Spawn(StructurePrefab);
+
+            Debug.Log("build mode engaged!!!");
+        }
 	}
+
+    private void OnTriggerEnter(Collider CollidedAsset)
+    {
+       if (CollidedAsset.CompareTag("GridCollider"))
+       {
+            if (StructurePrefab.GetComponent<WallController>().placeStatus != true)
+            {
+                StructurePrefab.GetComponent<Transform>().position = new Vector3(CollidedAsset.transform.position.x, CollidedAsset.transform.position.y, CollidedAsset.transform.position.z);
+            }
+       }
+    }
 }
---
+++ Assets/Scripts/WallController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallController : MonoBehaviour
+{
+    public Vector3 currentRot;
+    public bool placeStatus;
+
+    public Transform Wall_obj;
+
+	// Use this for initialization
+	void Start ()
+    {
+        currentRot.x = transform.rotation.x;
+        currentRot.y = transform.rotation.y;
+        currentRot.z = transform.rotation.z;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if (Input.GetKeyDown("space"))
+        {
+            placeStatus = true;
+        }
+	}
+}

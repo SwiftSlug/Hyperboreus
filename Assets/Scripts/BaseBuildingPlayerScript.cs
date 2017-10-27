20a6c65d46076c2d8c1fdb99236c3fe89c5db293
liampaddick <liampaddick2207@outlook.com>
2017-10-28 00:26:22 +0100

    Added Client side Materials
    Each Client can now choose a material to spawn. This is currently not visible between clients.

--- Assets/Scripts/BaseBuildingPlayerScript.cs
+++ Assets/Scripts/BaseBuildingPlayerScript.cs
@@ -9,10 +9,30 @@ public class BaseBuildingPlayerScript : NetworkBehaviour
     public Transform Object;
     public GameObject StructurePrefab;
 
+    public Material metal;
+    public Material stone;
+    public Material wood;
+
+    /*public Transform cube_obj;
+    public Transform Stairs_obj;
+    public Transform Doorway_obj;
+    public Transform Door_obj;
+    public Transform Floor_obj;
+    public Transform Ceiling_obj;
+    public Transform Ramps_obj;
+    public Transform BarbedWireTrap_obj;*/
+
     // Use this for initialization
     void Start () {
 		
 	}
+
+    [Command]
+    void CmdSpawnStructure()
+    {
+        StructurePrefab = (GameObject)Instantiate(StructurePrefab, Object.position, Object.rotation);
+        NetworkServer.Spawn(StructurePrefab);
+    }
 	
 	// Update is called once per frame
 	void Update ()
@@ -24,12 +44,30 @@ public class BaseBuildingPlayerScript : NetworkBehaviour
 
 		if (Input.GetKeyDown ("b"))
         {
-            StructurePrefab = (GameObject)Instantiate(StructurePrefab, Object.position, Object.rotation);
-            NetworkServer.Spawn(StructurePrefab);
+            CmdSpawnStructure();
 
             Debug.Log("build mode engaged!!!");
         }
-	}
+
+        if (Input.GetKeyDown("1") && (StructurePrefab.GetComponent<WallController>().placeStatus != true))
+        {
+            StructurePrefab.GetComponentInChildren<Renderer>().material = wood;
+
+            Debug.Log("ChangingToWood");
+        }
+        if (Input.GetKeyDown("2") && (StructurePrefab.GetComponent<WallController>().placeStatus != true))
+        {
+            StructurePrefab.GetComponentInChildren<Renderer>().material = stone;
+
+            Debug.Log("ChangingToStone");
+        }
+        if (Input.GetKeyDown("3") && (StructurePrefab.GetComponent<WallController>().placeStatus != true))
+        {
+            StructurePrefab.GetComponentInChildren<Renderer>().material = metal;
+
+            Debug.Log("ChangingToMetal");
+        }
+    }
 
     private void OnTriggerEnter(Collider CollidedAsset)    
     {

b42f16c565e6771efa5a75fb954fc529dfb6e896
liampaddick <liampaddick2207@outlook.com>
2017-10-30 19:46:45 +0000

    Materials and structures synced on server and client
    Players can now change materials and structure types using keys 1-3 to change between wood, stone and metal. 4-7 change between wall, stairs, doorway and door.
    
    When a player spawns a structure the spawned structure will be the same type and material chosen and will be visible on both clients and server.

--- Assets/Scripts/BaseBuildingPlayerScript.cs
+++ Assets/Scripts/BaseBuildingPlayerScript.cs
@@ -17,57 +17,32 @@ public class BaseBuildingPlayerScript : NetworkBehaviour
 
     public GameObject SpawnedStructure;
 
-    //public GameObject wall_obj;
-
-    //public Transform Wall_obj;
-    //public Transform Stairs_obj;
-    //public Transform Doorway_obj;
-    //public Transform Door_obj;
-    //public Transform Floor_obj;
-    //public Transform Ceiling_obj;
-    //public Transform Ramps_obj;
-    //public Transform BarbedWireTrap_obj;
-
     public int hitpoints = 100;
     public int structureSelector = 0;
 
     // Use this for initialization
     void Start ()
     {
-        ////disable colliders for placement actor choices
-        //Wall_obj.GetComponent<Collider>().enabled = false;
-        //Stairs_obj.GetComponent<Collider>().enabled = false;
-        //Doorway_obj.GetComponent<Collider>().enabled = false;
-        //Door_obj.GetComponent<Collider>().enabled = false;
-        //Floor_obj.GetComponent<Collider>().enabled = false;
-        //Ceiling_obj.GetComponent<Collider>().enabled = false;
-        //Ramps_obj.GetComponent<Collider>().enabled = false;
-        //BarbedWireTrap_obj.GetComponent<Collider>().enabled = false;
-
-        ////disable renderer for all except 
-        //Stairs_obj.GetComponent<Renderer>().enabled = false;
-        //Doorway_obj.GetComponent<Renderer>().enabled = false;
-        //Door_obj.GetComponent<Renderer>().enabled = false;
-        //Floor_obj.GetComponent<Renderer>().enabled = false;
-        //Ceiling_obj.GetComponent<Renderer>().enabled = false;
-        //Ramps_obj.GetComponent<Renderer>().enabled = false;
-        //BarbedWireTrap_obj.GetComponent<Renderer>().enabled = false;
     }
 
     [Command]
     void CmdSpawnStructure()
     {
         StructurePrefab = (GameObject)Instantiate(StructurePrefab, Object.position, Object.rotation);
-        //StructurePrefab.GetComponent<WallController>().SetMaterial(NeededMaterial);
         NetworkServer.Spawn(StructurePrefab);
     }
 
-    /*[Command]
-    void CmdSetMaterial(Material Mat)
+    [Command]
+    void CmdSetMaterial(int NeededMaterial)
     {
-        StructurePrefab.GetComponent<WallController>().SetMaterial(Mat);
+        StructurePrefab.GetComponent<WallController>().RpcSetMaterial(NeededMaterial);
+    }
 
-    }*/
+    [Command]
+    void CmdSetStructure(int structureSelector)
+    {
+        StructurePrefab.GetComponent<WallController>().RpcSetStructure(structureSelector);
+    }
 	
 	// Update is called once per frame
 	void Update ()
@@ -87,8 +62,7 @@ public class BaseBuildingPlayerScript : NetworkBehaviour
         if (Input.GetKeyDown("1") && (StructurePrefab.GetComponent<WallController>().placeStatus != true))
         {
             NeededMaterial = 0;
-            //StructurePrefab.GetComponentInChildren<Renderer>().material = NeededMaterial;
-            StructurePrefab.GetComponent<WallController>().RpcSetMaterial(NeededMaterial);
+            CmdSetMaterial(NeededMaterial);
             hitpoints = 100;
 
             Debug.Log("ChangingToWood");
@@ -96,8 +70,7 @@ public class BaseBuildingPlayerScript : NetworkBehaviour
         if (Input.GetKeyDown("2") && (StructurePrefab.GetComponent<WallController>().placeStatus != true))
         {
             NeededMaterial = 1;
-            //StructurePrefab.GetComponentInChildren<Renderer>().material = NeededMaterial;
-            StructurePrefab.GetComponent<WallController>().RpcSetMaterial(NeededMaterial);
+            CmdSetMaterial(NeededMaterial);
             hitpoints = 200;
 
             Debug.Log("ChangingToStone");
@@ -105,108 +78,38 @@ public class BaseBuildingPlayerScript : NetworkBehaviour
         if (Input.GetKeyDown("3") && (StructurePrefab.GetComponent<WallController>().placeStatus != true))
         {
             NeededMaterial = 2;
-            StructurePrefab.GetComponent<WallController>().RpcSetMaterial(NeededMaterial);
-            //StructurePrefab.GetComponentInChildren<Renderer>().material = NeededMaterial;
+            CmdSetMaterial(NeededMaterial);
             hitpoints = 300;
 
             Debug.Log("ChangingToMetal");
         }
 
-       //switch (structureSelector)
-       // {
-       //     case 0:
-       //         Wall_obj.GetComponent<BoxCollider>().enabled = true;
-       //         Wall_obj.GetComponent<Renderer>().enabled = true;
-
-       //         BarbedWireTrap_obj.GetComponent<BoxCollider>().enabled = false;
-       //         BarbedWireTrap_obj.GetComponent<Renderer>().enabled = false;
-
-       //         //if (Input.GetKeyDown("1") && (placeStatus != true))
-       //         //{
-       //         //    cube_obj.GetComponent<Renderer>().material = wood;
-       //         //    hitpoints = 100;
-       //         //}
-       //         //if (Input.GetKeyDown("2") && (placeStatus != true))
-       //         //{
-       //         //    cube_obj.GetComponent<Renderer>().material = stone;
-       //         //    hitpoints = 150;
-       //         //}
-       //         //if (Input.GetKeyDown("3") && (placeStatus != true))
-       //         //{
-       //         //    cube_obj.GetComponent<Renderer>().material = metal;
-       //         //    hitpoints = 200;
-       //         //}
-       //         break;
-
-       //     case 1:
-       //         Stairs_obj.GetComponent<BoxCollider>().enabled = true;
-       //         Stairs_obj.GetComponent<Renderer>().enabled = true;
-
-       //         Wall_obj.GetComponent<BoxCollider>().enabled = false;
-       //         Wall_obj.GetComponent<Renderer>().enabled = false;
-       //         break;
-
-       //     case 2:
-       //         Doorway_obj.GetComponent<BoxCollider>().enabled = true;
-       //         Doorway_obj.GetComponent<Renderer>().enabled = true;
-
-       //         Stairs_obj.GetComponent<BoxCollider>().enabled = false;
-       //         Stairs_obj.GetComponent<Renderer>().enabled = false;
-       //         break;
-
-       //     case 3:
-       //         Door_obj.GetComponent<BoxCollider>().enabled = true;
-       //         Door_obj.GetComponent<Renderer>().enabled = true;
-
-       //         Doorway_obj.GetComponent<BoxCollider>().enabled = false;
-       //         Doorway_obj.GetComponent<Renderer>().enabled = false;
-       //         break;
-
-       //     case 4:
-       //         Floor_obj.GetComponent<BoxCollider>().enabled = true;
-       //         Floor_obj.GetComponent<Renderer>().enabled = true;
-
-       //         Door_obj.GetComponent<BoxCollider>().enabled = false;
-       //         Door_obj.GetComponent<Renderer>().enabled = false;
-       //         break;
-
-       //     case 5:
-       //         Ceiling_obj.GetComponent<BoxCollider>().enabled = true;
-       //         Ceiling_obj.GetComponent<Renderer>().enabled = true;
-
-       //         Floor_obj.GetComponent<BoxCollider>().enabled = false;
-       //         Floor_obj.GetComponent<Renderer>().enabled = false;
-       //         break;
-
-       //     case 6:
-       //         Ramps_obj.GetComponent<BoxCollider>().enabled = true;
-       //         Ramps_obj.GetComponent<Renderer>().enabled = true;
-
-       //         Ceiling_obj.GetComponent<BoxCollider>().enabled = false;
-       //         Ceiling_obj.GetComponent<Renderer>().enabled = false;
-       //         break;
-
-       //     case 7:
-       //         BarbedWireTrap_obj.GetComponent<BoxCollider>().enabled = true;
-       //         BarbedWireTrap_obj.GetComponent<Renderer>().enabled = true;
-
-       //         Ramps_obj.GetComponent<BoxCollider>().enabled = false;
-       //         Ramps_obj.GetComponent<Renderer>().enabled = false;
-       //         break;
-       // }
-
-        if (Input.GetKeyDown("=") && (StructurePrefab.GetComponent<WallController>().placeStatus != true))
+        if (Input.GetKeyDown("4") && (StructurePrefab.GetComponent<WallController>().placeStatus != true))
         {
-            StructurePrefab.GetComponent<WallController>().ActiveStructure = structureSelector;
-            structureSelector++;
+            structureSelector = 0;
+            CmdSetStructure(structureSelector);
 
+            Debug.Log("ChangingToWall");
+        }
+        if (Input.GetKeyDown("5") && (StructurePrefab.GetComponent<WallController>().placeStatus != true))
+        {
+            structureSelector = 1;
+            CmdSetStructure(structureSelector);
 
-            if (structureSelector == 8)
-            {
-                structureSelector = 0;
-            }
+            Debug.Log("ChangingToStairs");
+        }
+        if (Input.GetKeyDown("6") && (StructurePrefab.GetComponent<WallController>().placeStatus != true))
+        {
+            structureSelector = 2;
+            CmdSetStructure(structureSelector);
 
-            //Debug.Log(structureSelector);
+            Debug.Log("ChangingToDoorway");
+        }
+        if (Input.GetKeyDown("7") && (StructurePrefab.GetComponent<WallController>().placeStatus != true))
+        {
+            structureSelector = 3;
+            CmdSetStructure(structureSelector);
+            Debug.Log("ChangingToDoor");
         }
     }
 
--- Assets/Scripts/WallController.cs
+++ Assets/Scripts/WallController.cs
@@ -12,56 +12,86 @@ public class WallController : NetworkBehaviour
     public GameObject Stairs_obj;
     public GameObject Doorway_obj;
     public GameObject Door_obj;
-    public GameObject Floor_obj;
-    public GameObject Ceiling_obj;
-    public GameObject Ramps_obj;
-    public GameObject BarbedWireTrap_obj;
 
     public Material metal;
     public Material stone;
     public Material wood;
 
-
-    public GameObject StructureToApplyMatTo;
-
-    public int ActiveStructure = 0;
-
     [ClientRpc]
     public void RpcSetMaterial(int MaterialToSet)
     {
-        //StructureToApplyMatTo.GetComponent<Renderer>().material = MaterialToApply;
 
         switch (MaterialToSet)
         {
             case 0:
                 Wall_obj.GetComponent<Renderer>().material = wood;
                 Stairs_obj.GetComponent<Renderer>().material = wood;
-                Door_obj.GetComponent<Renderer>().material = wood;
                 Doorway_obj.GetComponent<Renderer>().material = wood;
-                Floor_obj.GetComponent<Renderer>().material = wood;
-                Ceiling_obj.GetComponent<Renderer>().material = wood;
-                Ramps_obj.GetComponent<Renderer>().material = wood;
-                BarbedWireTrap_obj.GetComponent<Renderer>().material = wood;
+                Door_obj.GetComponent<Renderer>().material = wood;
+
                 break;
             case 1:
                 Wall_obj.GetComponent<Renderer>().material = stone;
                 Stairs_obj.GetComponent<Renderer>().material = stone;
-                Door_obj.GetComponent<Renderer>().material = stone;
                 Doorway_obj.GetComponent<Renderer>().material = stone;
-                Floor_obj.GetComponent<Renderer>().material = stone;
-                Ceiling_obj.GetComponent<Renderer>().material = stone;
-                Ramps_obj.GetComponent<Renderer>().material = stone;
-                BarbedWireTrap_obj.GetComponent<Renderer>().material = stone;
+                Door_obj.GetComponent<Renderer>().material = stone;
                 break;
             case 2:
                 Wall_obj.GetComponent<Renderer>().material = metal;
                 Stairs_obj.GetComponent<Renderer>().material = metal;
-                Door_obj.GetComponent<Renderer>().material = metal;
                 Doorway_obj.GetComponent<Renderer>().material = metal;
-                Floor_obj.GetComponent<Renderer>().material = metal;
-                Ceiling_obj.GetComponent<Renderer>().material = metal;
-                Ramps_obj.GetComponent<Renderer>().material = metal;
-                BarbedWireTrap_obj.GetComponent<Renderer>().material = metal;
+                Door_obj.GetComponent<Renderer>().material = metal;
+                break;
+        }
+    }
+
+    [ClientRpc]
+    public void RpcSetStructure(int StructureToSet)
+    {
+        switch (StructureToSet)
+        {
+            case 0:
+
+                Door_obj.GetComponent<Collider>().enabled = false;
+                Door_obj.GetComponent<Renderer>().enabled = false;
+
+                Wall_obj.GetComponent<Collider>().enabled = true;
+                Wall_obj.GetComponent<Renderer>().enabled = true;
+
+                Debug.Log("Wall");
+                break;
+
+            case 1:
+
+                Wall_obj.GetComponent<Collider>().enabled = false;
+                Wall_obj.GetComponent<Renderer>().enabled = false;
+
+                Stairs_obj.GetComponent<Collider>().enabled = true;
+                Stairs_obj.GetComponent<Renderer>().enabled = true;
+
+                Debug.Log("Stairs");
+                break;
+
+            case 2:
+
+                Stairs_obj.GetComponent<Collider>().enabled = false;
+                Stairs_obj.GetComponent<Renderer>().enabled = false;
+
+                Doorway_obj.GetComponent<Collider>().enabled = true;
+                Doorway_obj.GetComponent<Renderer>().enabled = true;
+
+                Debug.Log("Doorway");
+                break;
+
+            case 3:
+
+                Doorway_obj.GetComponent<Collider>().enabled = false;
+                Doorway_obj.GetComponent<Renderer>().enabled = false;
+
+                Door_obj.GetComponent<Collider>().enabled = true;
+                Door_obj.GetComponent<Renderer>().enabled = true;
+
+                Debug.Log("Doorway");
                 break;
         }
     }
@@ -77,21 +107,13 @@ public class WallController : NetworkBehaviour
         Stairs_obj.GetComponent<Collider>().enabled = false;
         Doorway_obj.GetComponent<Collider>().enabled = false;
         Door_obj.GetComponent<Collider>().enabled = false;
-        Floor_obj.GetComponent<Collider>().enabled = false;
-        Ceiling_obj.GetComponent<Collider>().enabled = false;
-        Ramps_obj.GetComponent<Collider>().enabled = false;
-        BarbedWireTrap_obj.GetComponent<Collider>().enabled = false;
 
         //disable renderer for all except Wall
         Stairs_obj.GetComponent<Renderer>().enabled = false;
         Doorway_obj.GetComponent<Renderer>().enabled = false;
         Door_obj.GetComponent<Renderer>().enabled = false;
-        Floor_obj.GetComponent<Renderer>().enabled = false;
-        Ceiling_obj.GetComponent<Renderer>().enabled = false;
-        Ramps_obj.GetComponent<Renderer>().enabled = false;
-        BarbedWireTrap_obj.GetComponent<Renderer>().enabled = false;
 
-        //SetMaterial(wood);
+        RpcSetMaterial(0);
     }
 
     // Update is called once per frame
@@ -101,118 +123,5 @@ public class WallController : NetworkBehaviour
         {
             placeStatus = true;
         }
-
-        switch (ActiveStructure)
-        {
-            case 0:
-                Wall_obj.GetComponent<Collider>().enabled = true;
-                Wall_obj.GetComponent<Renderer>().enabled = true;
-
-                BarbedWireTrap_obj.GetComponent<Collider>().enabled = false;
-                BarbedWireTrap_obj.GetComponent<Renderer>().enabled = false;
-
-                //StructureToApplyMatTo = Wall_obj;
-
-                Debug.Log("Wall");
-                break;
-
-            case 1:
-                Stairs_obj.GetComponent<Collider>().enabled = true;
-                Stairs_obj.GetComponent<Renderer>().enabled = true;
-
-                Wall_obj.GetComponent<Collider>().enabled = false;
-                Wall_obj.GetComponent<Renderer>().enabled = false;
-
-                //StructureToApplyMatTo = Stairs_obj;
-
-                Debug.Log("Stairs");
-                break;
-
-            case 2:
-                Doorway_obj.GetComponent<Collider>().enabled = true;
-                Doorway_obj.GetComponent<Renderer>().enabled = true;
-
-                Stairs_obj.GetComponent<Collider>().enabled = false;
-                Stairs_obj.GetComponent<Renderer>().enabled = false;
-
-               // StructureToApplyMatTo = Doorway_obj;
-
-                Debug.Log("Doorway");
-                break;
-
-            case 3:
-                Door_obj.GetComponent<Collider>().enabled = true;
-                Door_obj.GetComponent<Renderer>().enabled = true;
-
-                Doorway_obj.GetComponent<Collider>().enabled = false;
-                Doorway_obj.GetComponent<Renderer>().enabled = false;
-
-               // StructureToApplyMatTo = Door_obj;
-
-                Debug.Log("Door");
-                break;
-
-            case 4:
-                Floor_obj.GetComponent<Collider>().enabled = true;
-                Floor_obj.GetComponent<Renderer>().enabled = true;
-
-                Door_obj.GetComponent<Collider>().enabled = false;
-                Door_obj.GetComponent<Renderer>().enabled = false;
-
-               // StructureToApplyMatTo = Floor_obj;
-
-                Debug.Log("Floor");
-                break;
-
-            case 5:
-                Ceiling_obj.GetComponent<Collider>().enabled = true;
-                Ceiling_obj.GetComponent<Renderer>().enabled = true;
-
-                Floor_obj.GetComponent<Collider>().enabled = false;
-                Floor_obj.GetComponent<Renderer>().enabled = false;
-
-               // StructureToApplyMatTo = Ceiling_obj;
-
-                Debug.Log("Ceiling");
-                break;
-
-            case 6:
-                Ramps_obj.GetComponent<Collider>().enabled = true;
-                Ramps_obj.GetComponent<Renderer>().enabled = true;
-
-                Ceiling_obj.GetComponent<Collider>().enabled = false;
-                Ceiling_obj.GetComponent<Renderer>().enabled = false;
-
-               // StructureToApplyMatTo = Ramps_obj;
-
-                Debug.Log("Ramps");
-                break;
-
-            case 7:
-                BarbedWireTrap_obj.GetComponent<Collider>().enabled = true;
-                BarbedWireTrap_obj.GetComponent<Renderer>().enabled = true;
-
-                Ramps_obj.GetComponent<Collider>().enabled = false;
-                Ramps_obj.GetComponent<Renderer>().enabled = false;
-
-//              StructureToApplyMatTo = BarbedWireTrap_obj;
-
-                Debug.Log("BarbedWire");
-                break;
-        }
-
     }
-
-    /*public void SetMaterial(Material MaterialToApply)
-    {
-        //StructureToApplyMatTo.GetComponent<Renderer>().material = MaterialToApply;
-        Wall_obj.GetComponent<Renderer>().material = MaterialToApply;
-        Stairs_obj.GetComponent<Renderer>().material = MaterialToApply;
-        Door_obj.GetComponent<Renderer>().material = MaterialToApply;
-        Doorway_obj.GetComponent<Renderer>().material = MaterialToApply;
-        Floor_obj.GetComponent<Renderer>().material = MaterialToApply;
-        Ceiling_obj.GetComponent<Renderer>().material = MaterialToApply;
-        Ramps_obj.GetComponent<Renderer>().material = MaterialToApply;
-        BarbedWireTrap_obj.GetComponent<Renderer>().material = MaterialToApply;
-    }*/
 }

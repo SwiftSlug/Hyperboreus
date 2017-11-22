07cb5b5f44e41d33a2f4669ebf2339cd589e6f33
liampaddick <liampaddick2207@outlook.com>
2017-11-22 21:46:19 +0000

    Revert "Materials and structures synced on server and client"
    This reverts commit b42f16c565e6771efa5a75fb954fc529dfb6e896.

--- Assets/Scripts/BaseBuildingPlayerScript.cs
+++ Assets/Scripts/BaseBuildingPlayerScript.cs
@@ -17,34 +17,62 @@ public class BaseBuildingPlayerScript : NetworkBehaviour
 
     public GameObject SpawnedStructure;
 
+    //public GameObject wall_obj;
+
+    //public Transform Wall_obj;
+    //public Transform Stairs_obj;
+    //public Transform Doorway_obj;
+    //public Transform Door_obj;
+    //public Transform Floor_obj;
+    //public Transform Ceiling_obj;
+    //public Transform Ramps_obj;
+    //public Transform BarbedWireTrap_obj;
+
     public int hitpoints = 100;
     public int structureSelector = 0;
 
     // Use this for initialization
     void Start ()
     {
+<<<<<<< HEAD
         CmdSetMaterial(NeededMaterial);
         CmdSetStructure(structureSelector);
+=======
+        ////disable colliders for placement actor choices
+        //Wall_obj.GetComponent<Collider>().enabled = false;
+        //Stairs_obj.GetComponent<Collider>().enabled = false;
+        //Doorway_obj.GetComponent<Collider>().enabled = false;
+        //Door_obj.GetComponent<Collider>().enabled = false;
+        //Floor_obj.GetComponent<Collider>().enabled = false;
+        //Ceiling_obj.GetComponent<Collider>().enabled = false;
+        //Ramps_obj.GetComponent<Collider>().enabled = false;
+        //BarbedWireTrap_obj.GetComponent<Collider>().enabled = false;
+
+        ////disable renderer for all except 
+        //Stairs_obj.GetComponent<Renderer>().enabled = false;
+        //Doorway_obj.GetComponent<Renderer>().enabled = false;
+        //Door_obj.GetComponent<Renderer>().enabled = false;
+        //Floor_obj.GetComponent<Renderer>().enabled = false;
+        //Ceiling_obj.GetComponent<Renderer>().enabled = false;
+        //Ramps_obj.GetComponent<Renderer>().enabled = false;
+        //BarbedWireTrap_obj.GetComponent<Renderer>().enabled = false;
+>>>>>>> parent of b42f16c... Materials and structures synced on server and client
     }
 
     [Command]
     void CmdSpawnStructure()
     {
         StructurePrefab = (GameObject)Instantiate(StructurePrefab, Object.position, Object.rotation);
+        //StructurePrefab.GetComponent<WallController>().SetMaterial(NeededMaterial);
         NetworkServer.Spawn(StructurePrefab);
     }
 
-    [Command]
-    void CmdSetMaterial(int NeededMaterial)
+    /*[Command]
+    void CmdSetMaterial(Material Mat)
     {
-        StructurePrefab.GetComponent<WallController>().RpcSetMaterial(NeededMaterial);
-    }
+        StructurePrefab.GetComponent<WallController>().SetMaterial(Mat);
 
-    [Command]
-    void CmdSetStructure(int structureSelector)
-    {
-        StructurePrefab.GetComponent<WallController>().RpcSetStructure(structureSelector);
-    }
+    }*/
 	
 	// Update is called once per frame
 	void Update ()
@@ -200,7 +228,8 @@ public class BaseBuildingPlayerScript : NetworkBehaviour
         if (Input.GetKeyDown("1") && (StructurePrefab.GetComponent<WallController>().placeStatus != true))
         {
             NeededMaterial = 0;
-            CmdSetMaterial(NeededMaterial);
+            //StructurePrefab.GetComponentInChildren<Renderer>().material = NeededMaterial;
+            StructurePrefab.GetComponent<WallController>().RpcSetMaterial(NeededMaterial);
             hitpoints = 100;
 
             Debug.Log("ChangingToWood");
@@ -208,7 +237,8 @@ public class BaseBuildingPlayerScript : NetworkBehaviour
         if (Input.GetKeyDown("2") && (StructurePrefab.GetComponent<WallController>().placeStatus != true))
         {
             NeededMaterial = 1;
-            CmdSetMaterial(NeededMaterial);
+            //StructurePrefab.GetComponentInChildren<Renderer>().material = NeededMaterial;
+            StructurePrefab.GetComponent<WallController>().RpcSetMaterial(NeededMaterial);
             hitpoints = 200;
 
             Debug.Log("ChangingToStone");
@@ -216,38 +246,108 @@ public class BaseBuildingPlayerScript : NetworkBehaviour
         if (Input.GetKeyDown("3") && (StructurePrefab.GetComponent<WallController>().placeStatus != true))
         {
             NeededMaterial = 2;
-            CmdSetMaterial(NeededMaterial);
+            StructurePrefab.GetComponent<WallController>().RpcSetMaterial(NeededMaterial);
+            //StructurePrefab.GetComponentInChildren<Renderer>().material = NeededMaterial;
             hitpoints = 300;
 
             Debug.Log("ChangingToMetal");
         }
 
-        if (Input.GetKeyDown("4") && (StructurePrefab.GetComponent<WallController>().placeStatus != true))
-        {
-            structureSelector = 0;
-            CmdSetStructure(structureSelector);
+       //switch (structureSelector)
+       // {
+       //     case 0:
+       //         Wall_obj.GetComponent<BoxCollider>().enabled = true;
+       //         Wall_obj.GetComponent<Renderer>().enabled = true;
 
-            Debug.Log("ChangingToWall");
-        }
-        if (Input.GetKeyDown("5") && (StructurePrefab.GetComponent<WallController>().placeStatus != true))
-        {
-            structureSelector = 1;
-            CmdSetStructure(structureSelector);
+       //         BarbedWireTrap_obj.GetComponent<BoxCollider>().enabled = false;
+       //         BarbedWireTrap_obj.GetComponent<Renderer>().enabled = false;
 
-            Debug.Log("ChangingToStairs");
-        }
-        if (Input.GetKeyDown("6") && (StructurePrefab.GetComponent<WallController>().placeStatus != true))
-        {
-            structureSelector = 2;
-            CmdSetStructure(structureSelector);
+       //         //if (Input.GetKeyDown("1") && (placeStatus != true))
+       //         //{
+       //         //    cube_obj.GetComponent<Renderer>().material = wood;
+       //         //    hitpoints = 100;
+       //         //}
+       //         //if (Input.GetKeyDown("2") && (placeStatus != true))
+       //         //{
+       //         //    cube_obj.GetComponent<Renderer>().material = stone;
+       //         //    hitpoints = 150;
+       //         //}
+       //         //if (Input.GetKeyDown("3") && (placeStatus != true))
+       //         //{
+       //         //    cube_obj.GetComponent<Renderer>().material = metal;
+       //         //    hitpoints = 200;
+       //         //}
+       //         break;
 
-            Debug.Log("ChangingToDoorway");
-        }
-        if (Input.GetKeyDown("7") && (StructurePrefab.GetComponent<WallController>().placeStatus != true))
+       //     case 1:
+       //         Stairs_obj.GetComponent<BoxCollider>().enabled = true;
+       //         Stairs_obj.GetComponent<Renderer>().enabled = true;
+
+       //         Wall_obj.GetComponent<BoxCollider>().enabled = false;
+       //         Wall_obj.GetComponent<Renderer>().enabled = false;
+       //         break;
+
+       //     case 2:
+       //         Doorway_obj.GetComponent<BoxCollider>().enabled = true;
+       //         Doorway_obj.GetComponent<Renderer>().enabled = true;
+
+       //         Stairs_obj.GetComponent<BoxCollider>().enabled = false;
+       //         Stairs_obj.GetComponent<Renderer>().enabled = false;
+       //         break;
+
+       //     case 3:
+       //         Door_obj.GetComponent<BoxCollider>().enabled = true;
+       //         Door_obj.GetComponent<Renderer>().enabled = true;
+
+       //         Doorway_obj.GetComponent<BoxCollider>().enabled = false;
+       //         Doorway_obj.GetComponent<Renderer>().enabled = false;
+       //         break;
+
+       //     case 4:
+       //         Floor_obj.GetComponent<BoxCollider>().enabled = true;
+       //         Floor_obj.GetComponent<Renderer>().enabled = true;
+
+       //         Door_obj.GetComponent<BoxCollider>().enabled = false;
+       //         Door_obj.GetComponent<Renderer>().enabled = false;
+       //         break;
+
+       //     case 5:
+       //         Ceiling_obj.GetComponent<BoxCollider>().enabled = true;
+       //         Ceiling_obj.GetComponent<Renderer>().enabled = true;
+
+       //         Floor_obj.GetComponent<BoxCollider>().enabled = false;
+       //         Floor_obj.GetComponent<Renderer>().enabled = false;
+       //         break;
+
+       //     case 6:
+       //         Ramps_obj.GetComponent<BoxCollider>().enabled = true;
+       //         Ramps_obj.GetComponent<Renderer>().enabled = true;
+
+       //         Ceiling_obj.GetComponent<BoxCollider>().enabled = false;
+       //         Ceiling_obj.GetComponent<Renderer>().enabled = false;
+       //         break;
+
+       //     case 7:
+       //         BarbedWireTrap_obj.GetComponent<BoxCollider>().enabled = true;
+       //         BarbedWireTrap_obj.GetComponent<Renderer>().enabled = true;
+
+       //         Ramps_obj.GetComponent<BoxCollider>().enabled = false;
+       //         Ramps_obj.GetComponent<Renderer>().enabled = false;
+       //         break;
+       // }
+
+        if (Input.GetKeyDown("=") && (StructurePrefab.GetComponent<WallController>().placeStatus != true))
         {
-            structureSelector = 3;
-            CmdSetStructure(structureSelector);
-            Debug.Log("ChangingToDoor");
+            StructurePrefab.GetComponent<WallController>().ActiveStructure = structureSelector;
+            structureSelector++;
+
+
+            if (structureSelector == 8)
+            {
+                structureSelector = 0;
+            }
+
+            //Debug.Log(structureSelector);
         }
     }
 

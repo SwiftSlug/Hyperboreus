ed79df578131ac7bd7d618abec1a926c8c26bd8e
Jayboyo <jaymun123@gmail.com>
2018-01-18 12:23:52 +0000

    Medic Ability update
    Medic ability has bee updated to fire correclty with the coodlown and charge times. It also now has its on seperate line renderer, which works with the firing line renderer.

--- Assets/Scripts/PlayerSkills/MedicSkill.cs
+++ Assets/Scripts/PlayerSkills/MedicSkill.cs
@@ -4,22 +4,15 @@ using UnityEngine;
 
 public class MedicSkill : Skill {
 
-    //LineRenderer laser = gameobject.AddComponent<LineRenderer>();
-
     //  Line renderer used to represent the laser sight of the syringe
     LineRenderer laser;
     //  Time it takes to charge the syringe shot
     public float chargeTime;
+    //  The current time spent charging the ability
+    float currentChargeTime;
 
-    //GameObject syringe = GameObject.Instantiate((GameObject)Prefebs.Load("MedicalSyringe"));
-
-    //Object syringe = AssetDatabase.LoadAssetAtPath("Assets/something.prefab", typeof(GameObject));
-
-    //GameObject instance = Instantiate(Resources.Load("enemy", )) as GameObject;
-
+    //  The game object used to instantiate the fired syringe
     GameObject syringe;
-
-    //public GameObject syringeModel;
 
     public override void Init()
     {
@@ -33,41 +26,40 @@ public class MedicSkill : Skill {
             //  Sets the chargetime to 2 seconds if no other value is set
             chargeTime = 1.0f;
         }
-
+        
+        //  Add the line renderer component to the player
         laser = playerReference.AddComponent<LineRenderer>();
+        //  Set the laser to default off
         laser.enabled = false;
-
+        //  Load the syringe gameobject
         syringe = Instantiate(Resources.Load("MedicalSyringe", typeof(GameObject))) as GameObject;
 
-        //syringe = GetComponent<>();
-        //Resources.Load("Assets//Prefabs//MedicalSyringe.prefab");
-        //Instantiate(Resources.Load("MedicalSyringe"), transform.position, transform.rotation);
-        //syringe = Instantiate(Resources.Load("MedicalSyringe"), transform.position, transform.rotation) as GameObject;
     }
 
     public override bool SkillAction()
     {
-
         if (Time.time > lastUsedTime + cooldown)
         {
             laser.SetPosition(0, playerReference.transform.position);           //  Sets laser start location to player
             laser.SetPosition(1, playerReference.transform.forward * 1000);     //  Sets laser end to 1000 in front of player
             laser.enabled = true;   //  Turn laser on
 
-            if (buttonDownTime > chargeTime)
-            {
-                //Debug.Log("Skill Activate");
-                //GetComponent<PlayerStats>().currentHealth = GetComponent<PlayerStats>().currentHealth + 50;
+            //  Increase the charge time
+            currentChargeTime += Time.deltaTime;
 
+            if (currentChargeTime > chargeTime)
+            {
                 //  Fire a syringe
                 if (syringe)
                 {
+                    //  Create the syringe game object
                     Instantiate(syringe, transform.position, transform.rotation);
                 }
-
-                laser.enabled = false;  //  Turn laser off
-                lastUsedTime = Time.time;
-                return true;
+                
+                currentChargeTime = 0.0f;   //  Reset the current charge time
+                laser.enabled = false;      //  Turn laser off
+                lastUsedTime = Time.time;   //  Set last firing time
+                return true;    
             }
 
             
@@ -77,8 +69,9 @@ public class MedicSkill : Skill {
 
     public override void buttonRelease()
     {
-        laser.enabled = false;  //  Turn laser off
-        Debug.Log("Button released");
+        currentChargeTime = 0.0f;       //  Reset the current charge time
+        laser.enabled = false;          //  Turn laser off
+        //Debug.Log("Button released");
     }
 
 }

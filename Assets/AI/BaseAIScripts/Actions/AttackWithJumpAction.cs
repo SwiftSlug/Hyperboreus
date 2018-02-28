b6777733825e68020d21fc22645d36eb07d1adbc
liampaddick <liampaddick2207@outlook.com>
2018-02-28 02:44:58 +0000

    Made changes to trap
    Trap now damages multiple enemies at once, this and resetting speeds will need to be tweaked as both behaviours are temperamental when very high amounts of enemies are in the trap.
    
    Removed debug from jump attack script.

--- Assets/AI/BaseAIScripts/Actions/AttackWithJumpAction.cs
+++ Assets/AI/BaseAIScripts/Actions/AttackWithJumpAction.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "PluggableAI/Actions/AttackWithJump")]
 public class AttackWithJumpAction : Action
 {
-
     public override void Act(StateController controller)
     {
         CrawlerAttackWithJump(controller);
@@ -16,33 +15,40 @@ public class AttackWithJumpAction : Action
 
         controller.navMeshAgent.destination = controller.target.transform.position;
 
-        if ((controller.transform.position - controller.target.transform.position).magnitude < controller.stopDistance)
+        if (controller.gameObject.GetComponent<AIStats>().isTrapped == false)
         {
-            controller.navMeshAgent.destination = controller.transform.position;    //  Target close enough stop moving        
-
-            if (Time.time > (controller.lastAttack + controller.attackCooldown)) //  Call attack only every 5 seconds
+            if ((controller.transform.position - controller.target.transform.position).magnitude < controller.stopDistance)
             {
-                controller.target.GetComponent<PlayerStats>().CmdDamage(controller.attackDamage);
-                controller.lastAttack = Time.time;
-                //Debug.Log("Attack");
+                controller.navMeshAgent.destination = controller.transform.position;    //  Target close enough stop moving        
+
+                if (Time.time > (controller.lastAttack + controller.attackCooldown)) //  Call attack only every 5 seconds
+                {
+                    controller.target.GetComponent<PlayerStats>().CmdDamage(controller.attackDamage);
+                    controller.lastAttack = Time.time;
+                    //Debug.Log("Attack");
+                }
+
+            }
+            else
+            {
+                controller.navMeshAgent.destination = controller.target.transform.position; //  Target too far move to target
             }
 
+            if (!controller.animator.GetCurrentAnimatorStateInfo(0).IsName("EnemyJumpAnimation"))   //  Is the jump animation running
+            {
+                controller.navMeshAgent.speed = controller.runSpeed;
+            }
+            else
+            {
+                controller.navMeshAgent.speed = controller.runSpeed * 100;
+            }
+
+            JumpAttack(controller);
         }
         else
         {
-            controller.navMeshAgent.destination = controller.target.transform.position; //  Target too far move to target
+            //Debug.Log("I can't jump, I'm trapped!");
         }
-
-        if (!controller.animator.GetCurrentAnimatorStateInfo(0).IsName("EnemyJumpAnimation"))   //  Is the jump animation running
-        {
-            controller.navMeshAgent.speed = controller.runSpeed;
-        }
-        else
-        {
-            controller.navMeshAgent.speed = controller.runSpeed * 100;
-        }
-
-        JumpAttack(controller);
 
     }
 
--- Assets/Scripts/PlayerSkills/BarbedWireTrap.cs
+++ Assets/Scripts/PlayerSkills/BarbedWireTrap.cs
@@ -5,13 +5,16 @@ using UnityEngine.Networking;
 
 public class BarbedWireTrap : NetworkBehaviour
 {
-
     public GameObject CollidedEnemy = null;
     float DamageTimer = 0.0f;
     float DestructionTimer = 0.0f;
+    float InitialWalkSpeed;
     float InitialRunSpeed;
+    float InitialNavMeshAgentSpeed;
+    public float SpeedToSet;
     public bool EnemyColliding = false;
-    public GameObject CollidedEnemyTest = null;
+
+    public List<GameObject> CollidedEnemiesList = new List<GameObject>();
 
     // Use this for initialization
     void Start()
@@ -28,6 +31,18 @@ public class BarbedWireTrap : NetworkBehaviour
             if (DamageTimer >= 1.0f)
             {
                 CollidedEnemy.GetComponent<AIStats>().CmdDamage(5);
+
+                for (int i = 0; i < CollidedEnemiesList.Count; i++)
+                {
+                    if (CollidedEnemiesList[i] != null)
+                    {
+                        CollidedEnemiesList[i].GetComponent<AIStats>().CmdDamage(5);
+                    }
+                    else
+                    {
+                        CollidedEnemiesList.RemoveAt(i);
+                    }
+                }
                 DamageTimer = 0.0f;
             }
             else
@@ -52,10 +67,19 @@ public class BarbedWireTrap : NetworkBehaviour
         if (collidedAsset.gameObject.CompareTag("Enemy") == true)
         {
             EnemyColliding = true;
+            CollidedEnemiesList.Add(collidedAsset.gameObject);
+
             CollidedEnemy = collidedAsset.gameObject;
             collidedAsset.gameObject.GetComponent<AIStats>().CmdSetTrapped(true);
+
+            InitialWalkSpeed = CollidedEnemy.gameObject.GetComponent<StateController>().walkSpeed;
             InitialRunSpeed = CollidedEnemy.gameObject.GetComponent<StateController>().runSpeed;
-            collidedAsset.gameObject.GetComponent<StateController>().runSpeed = 0.0005f;
+            InitialNavMeshAgentSpeed = CollidedEnemy.gameObject.GetComponent<StateController>().navMeshAgent.speed;
+
+
+            collidedAsset.gameObject.GetComponent<StateController>().walkSpeed = SpeedToSet;
+            collidedAsset.gameObject.GetComponent<StateController>().runSpeed = SpeedToSet;
+            collidedAsset.gameObject.GetComponent<StateController>().navMeshAgent.speed = SpeedToSet;
         }
     }
     private void OnTriggerExit(Collider collidedAsset)
@@ -65,7 +89,20 @@ public class BarbedWireTrap : NetworkBehaviour
             EnemyColliding = false;
             Debug.Log("Enemy No Longer Colliding");
             collidedAsset.gameObject.GetComponent<AIStats>().CmdSetTrapped(false);
+
+            collidedAsset.gameObject.GetComponent<StateController>().walkSpeed = InitialWalkSpeed;
             collidedAsset.gameObject.GetComponent<StateController>().runSpeed = InitialRunSpeed;
+            collidedAsset.gameObject.GetComponent<StateController>().navMeshAgent.speed = InitialNavMeshAgentSpeed;
+
+            for(int i = 0; i < CollidedEnemiesList.Count; i++)
+            {
+                if (CollidedEnemiesList[i] == collidedAsset)
+                {
+                    CollidedEnemiesList.RemoveAt(i);
+                }
+            }
+
+
             CollidedEnemy = null;
         }
     }
@@ -75,9 +112,22 @@ public class BarbedWireTrap : NetworkBehaviour
     {
         if (CollidedEnemy != null)
         {
-            CollidedEnemy.gameObject.GetComponent<StateController>().runSpeed = InitialRunSpeed;
-            CollidedEnemy.gameObject.GetComponent<AIStats>().CmdSetTrapped(false);
-            CollidedEnemy = null;
+            //CollidedEnemy.gameObject.GetComponent<AIStats>().CmdSetTrapped(false);
+
+            for (int i = 0; i < CollidedEnemiesList.Count; i++)
+            {
+                CollidedEnemiesList[i].gameObject.GetComponent<StateController>().walkSpeed = InitialWalkSpeed;
+                CollidedEnemiesList[i].gameObject.GetComponent<StateController>().runSpeed = InitialRunSpeed;
+                CollidedEnemiesList[i].gameObject.GetComponent<StateController>().navMeshAgent.speed = InitialNavMeshAgentSpeed;
+                CollidedEnemiesList[i].gameObject.GetComponent<AIStats>().CmdSetTrapped(false);
+            }
+            CollidedEnemiesList.Clear();
+
+            //CollidedEnemy.gameObject.GetComponent<StateController>().walkSpeed = InitialWalkSpeed;
+            //CollidedEnemy.gameObject.GetComponent<StateController>().runSpeed = InitialRunSpeed;
+            //CollidedEnemy.gameObject.GetComponent<StateController>().navMeshAgent.speed = InitialNavMeshAgentSpeed;
+
+            //CollidedEnemy = null;
         }
         NetworkServer.Destroy(gameObject);
     }
@@ -87,9 +137,22 @@ public class BarbedWireTrap : NetworkBehaviour
     {
         if (CollidedEnemy != null)
         {
-            CollidedEnemy.gameObject.GetComponent<StateController>().runSpeed = InitialRunSpeed;
-            CollidedEnemy.gameObject.GetComponent<AIStats>().CmdSetTrapped(false);
-            CollidedEnemy = null;
+            //CollidedEnemy.gameObject.GetComponent<AIStats>().CmdSetTrapped(false);
+
+            for (int i = 0; i < CollidedEnemiesList.Count; i++)
+            {
+                CollidedEnemiesList[i].gameObject.GetComponent<StateController>().walkSpeed = InitialWalkSpeed;
+                CollidedEnemiesList[i].gameObject.GetComponent<StateController>().runSpeed = InitialRunSpeed;
+                CollidedEnemiesList[i].gameObject.GetComponent<StateController>().navMeshAgent.speed = InitialNavMeshAgentSpeed;
+                CollidedEnemiesList[i].gameObject.GetComponent<AIStats>().CmdSetTrapped(false);
+            }
+            CollidedEnemiesList.Clear();
+
+            //CollidedEnemy.gameObject.GetComponent<StateController>().walkSpeed = InitialWalkSpeed;
+            //CollidedEnemy.gameObject.GetComponent<StateController>().runSpeed = InitialRunSpeed;
+            //CollidedEnemy.gameObject.GetComponent<StateController>().navMeshAgent.speed = InitialNavMeshAgentSpeed;
+
+            //CollidedEnemy = null;
         }
         Destroy(gameObject);
     }
